a17f236fa3e7577281837c9692724d5cfce4ee44
Sara Patel <gallegosara@hotmail.com>
2021-04-06 18:56:21 -0400

    Added event

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,27 +3,44 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-const cells = document.getElementsByTagName('tbody');
-
 var rowNames = ["datetime","city","state", "country", "shape", "durationMinutes", "comments"]
 
-function createTable(tableData) {
+function createTable(tableData, date ) {
     var tableBody = document.getElementsByTagName('tbody')[0]
-  
+    tableBody.innerHTML = ''; 
     tableData.forEach(function(rowData) {
+      
         var row = document.createElement('tr');
+        
+        if (date === "" || rowData["datetime"] === date) {
+            rowNames.forEach(function(d){
+
+                var cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(rowData[d]));
+                row.appendChild(cell);
     
-          rowNames.forEach(function(d){
-              var cell = document.createElement('td');
-              cell.appendChild(document.createTextNode(rowData[d]));
-              row.appendChild(cell);
-  
-          })
-    
-        tableBody.appendChild(row);
+            })
+      
+          tableBody.appendChild(row);
+
+        }
+
       });
+ 
+
   
   }
 
+createTable(tableData, "")
 
-createTable(tableData)
+
+const input = document.querySelector('input');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+    console.log(e)
+    console.log(e.target.value)
+    createTable(tableData, e.target.value )
+  }
+
