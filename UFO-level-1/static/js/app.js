522391a73eaa8879173d39efdeca97be590b9f34
Sara Patel <gallegosara@hotmail.com>
2021-04-06 18:12:16 -0400

    added loop

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,3 +7,23 @@ const cells = document.getElementsByTagName('tbody');
 
 var rowNames = ["datetime","city","state", "country", "shape", "durationMinutes", "comments"]
 
+function createTable(tableData) {
+    var tableBody = document.getElementsByTagName('tbody')[0]
+  
+    tableData.forEach(function(rowData) {
+        var row = document.createElement('tr');
+    
+          rowNames.forEach(function(d){
+              var cell = document.createElement('td');
+              cell.appendChild(document.createTextNode(rowData[d]));
+              row.appendChild(cell);
+  
+          })
+    
+        tableBody.appendChild(row);
+      });
+  
+  }
+
+
+createTable(tableData)
