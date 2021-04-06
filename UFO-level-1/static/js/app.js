f070a97c969e561354fc830d841cae848a449e01
Sara Patel <gallegosara@hotmail.com>
2021-04-06 18:10:48 -0400

    added row names

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,8 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+const cells = document.getElementsByTagName('tbody');
+
+var rowNames = ["datetime","city","state", "country", "shape", "durationMinutes", "comments"]
+
--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function printName(name) {
+    console.log(name);
+  }
+
+for (var i = 0; i < tableData.length; i++) {
+    console.log(tableData[i].city);
+  }
